a5c7d6dec39f66b64b477718ed9270c54c24d474
Daiwei Lin <daiwei.lin@edu.uwaterloo.ca>
2019-04-01 14:51:46 -0400

    implementation CART w/ categorical data
    optimized using nested list instead of pandas dataframe

--- code/Decisiontree.py
+++ code/Decisiontree.py
@@ -5,23 +5,29 @@ import pandas as pd
 import Tree
 
 
-class DecisionTree:
+class DecisionTreeNode:
     def __init__(self, data):
         self.data = data
-        self.tree = Tree()
+
+        self.left = None
+        self.right = None
+        self.isleaf = False
+        self.split_val = None
+        self.split_feature = None
 
 
-    def find_split_val(self,data):
+
+    def find_split_val(self):
         # Find best split value
-        class_values = list(set(row[-1] for row in data))
+        class_values = list(set(self.data.iloc[:,-1]))
         b_index, b_value, b_score, b_groups = 999, 999, 999, None
-        for index in range(len(data[0]) - 1):
-            for row in data:
-                groups = self.split(index, row[index], data)
+        for feature in self.data.columns:
+            for _, row in self.data.iterrows():
+                groups = self.split(feature, row[feature], self.data)
                 gini = self.gini_index(groups, class_values)
-                print('X%d < %.3f Gini=%.3f' % ((index + 1), row[index], gini))
+                print('X%d < %.3f Gini=%.3f' % ((feature + 1), row[feature], gini))
                 if gini < b_score:
-                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+                    b_index, b_value, b_score, b_groups = feature, row[feature], gini, groups
         return {'index': b_index, 'value': b_value, 'groups': b_groups}
 
     def split(self, val, feature, data):
@@ -61,9 +67,11 @@ class DecisionTree:
             gini += (1.0 - score) * (size / n_instances) # *0.5
         return gini
 
-    def train(self):
+    def train(self, data):
         # grow a tree
-        return None
+        root = self.get_split(data)
+        self.split(root, 1)
+        return root
 
     def predict(self, data):
         pred = np.zeros((data.shape[0], 1))
@@ -76,7 +84,16 @@ class DecisionTree:
 
 
 if __name__ == '__main__':
-    data = pd.read_csv("")
-    dt = DecisionTree(data)
-    dt.train()
-    dt.predict()
+    data = pd.DataFrame([[2.771244718,1.784783929,0],
+	[1.728571309,1.169761413,0],
+	[3.678319846,2.81281357,0],
+	[3.961043357,2.61995032,0],
+	[2.999208922,2.209014212,0],
+	[7.497545867,3.162953546,1],
+	[9.00220326,3.339047188,1],
+	[7.444542326,0.476683375,1],
+	[10.12493903,3.234550982,1],
+	[6.642287351,3.319983761,1]])
+
+    dt = DecisionTreeNode(data)
+    dt.find_split_val()
